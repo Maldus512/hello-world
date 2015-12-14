5c0c6028372b3b4e282880acd3ac3ff3192ffdf0
Maldus512 <mattia512maldini@gmail.com>
2015-12-14 19:30:52 +0100

    New customization feature

--- prova.c
+++ prova.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello world\n");
+int main(int argc, char* argv[]){
+    if(argc == 1) printf("hello world\n");
+    else if( argc > 1 ) printf("hello world, %s\n", (char*)argv[1]);
     return 0;
 }
